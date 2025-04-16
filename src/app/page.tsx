1c342df59c3ab58f3c075918b0c813cddf844ccc
João Victor dos Santos Silva <joao.viictorss31@gmail.com>
2025-04-16 01:07:04 -0300

    style: adds additional customizations to shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <div>
-      <p className="text-rose-500">
-        Hello world!
-      </p>
+      Hello world!
     </div>
   );
 }
